5ff54d5bb7429529bf7700762c29b966ae0521b1
lGsusl <jesuslopez@iesmontsia.org>
2016-05-02 12:29:24 +0200

    Serret es gay, puto subnormal de part de Jose Yuju

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -5,7 +5,11 @@ import java.*;
  */
 public class Biblioteca {
 
+    /* ---------- MENÚ ---------- */
+
     public static int menu(String[] opcions, String missatgeEscollirOpcio) {
+
+        int opcio;
 
         for (int i=0; i<opcions.length; i++){
 
@@ -14,8 +18,56 @@ public class Biblioteca {
         }
 
         imprimir(missatgeEscollirOpcio);
+        opcio = llegirEnter("Introdueix una opció: ");
+
+        return opcio;
 
     }
+
+    /* ---------- FUNCIONS DE LECTURA ----------- */
+
+    public static int llegirEnter(String missatge){
+
+        Scanner sc = new Scanner(System.in);
+        int n=0;
+        boolean correcte;
+
+        imprimir(missatge);
+
+        do{
+
+            correcte = sc.hasNextInt();
+
+            if (correcte){
+
+                n = sc.nextInt();
+
+            }else{
+
+                sc.nextLine();
+
+            }
+        }while(!correcte);
+
+        return n;
+
+    }
+
+    public static String llegirLinia(String missatge){
+
+        Scanner sc = new Scanner (System.in);
+        String linea;
+
+        imprimir(missatge);
+
+        linea = sc.nextLine();
+
+        return linea;
+
+    }
+
+
+    /* ---------- FUNCIONS D'IMPRESSIÓ ---------- */
 
     public static void imprimir(String text){
 
@@ -23,6 +75,17 @@ public class Biblioteca {
 
     }
 
+    public static void imprimirln(){
+
+        System.out.println("");
+
+    }
+
+    public static void imprimirln(String text){
+
+        System.out.println(text);
+
+    }
 
 
 }
