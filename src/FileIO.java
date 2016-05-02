ec9520316633d194fec9ed929d3e651597497225
serret96 <ruben.serret@gmail.com>
2016-05-02 10:44:56 +0200

    Salutacions

--- src/FileIO.java
+++ src/FileIO.java
@@ -2,4 +2,10 @@
  * Created by RUBEN on 02/05/2016.
  */
 public class FileIO {
+
+    public static String[] getIdUsuaris(String fitxer);
+    public static Usuari getUsuari(String fitxer, String idUsuari);
+    public static Usuari[] getUsuaris(String fitxer);
+    public static boolean guardarUsuari(String fitxer, Usuari usuari);
+
 }
