30179c9c82ddc537e4f20f9b5441ae3d42d0505e
OscarOliver <oscaroliverobiol@hotmail.es>
2016-05-02 11:36:40 +0200

    Actualització Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,20 @@
-import java.*;
 /**
- * Created by RUBEN on 02/05/2016.
+ * Created by Oscar on 02/05/2016.
  */
+
 public class Principal {
-    public static void man(String[] args) {
+    public static void main(String[] args) {
         new Principal().inici();
     }
 
-    void inici() {
-
-
+    public void inici() {
+        String[] opcionsMenu = {
+				"Inserir usuari",
+				"Consultar usuari",
+				"Mostrar usuaris",			// Mostra nomes usuaris valids. Camps: Nom, Cognoms, email
+				"Modificar usuari",
+				"Borrar usuari"
+		};
     }
 }
 
