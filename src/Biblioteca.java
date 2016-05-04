7495c9e48f57f8e2c1b31c58f4b4d60e444f8ee7
OscarOliver <oscaroliverobiol@hotmail.es>
2016-05-04 11:27:09 +0200

    Actualització funció menu Afegit: imprimirTaula(...) Actualització de Principal

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -7,15 +7,16 @@ public class Biblioteca {
     /* ---------- MENÚ ---------- */
 
     public static int menu(String[] opcions, String missatgeEscollirOpcio) {
-
         int opcio;
 
-        for (int i=0; i<opcions.length; i++) {
-            if ((i+1) < 10) imprimir(" ");
-            imprimirln((i+1) + ". " + opcions[i]);
+        for (int i=0, n=1; i<opcions.length; i++, n++) {
+            if (n < 10) imprimir(" ");
+            imprimirln(" " + n + ". " + opcions[i]);
         }
-        opcio = llegirEnter(missatgeEscollirOpcio);
 
+		do {
+			opcio = llegirEnter(missatgeEscollirOpcio);
+		} while (opcio < 1 || opcio > opcions.length);
         return opcio;
 
     }
@@ -84,4 +85,63 @@ public class Biblioteca {
     }
 
 
+	public static void imprimirTaula(String[] titols, String[][] contingut) {
+		imprimirTaula(titols, contingut, 3);
+	}
+
+	public static void imprimirTaula(String[] titols, String[][] contingut, int espaiEntreColumnes) {
+		if (titols == null || contingut == null) {
+			imprimirln("El contingut o els titols no poden ser 'null'");
+			return;
+		}
+		if (titols.length != contingut[0].length) {
+			imprimirln("El contingut no encaixa amb el numero de titols.");
+			return;
+		}
+
+		int[] colMaxs = new int[titols.length];
+		for (int i = 0; i < titols.length; i++) {
+			colMaxs[i] = titols[i].length();
+		}
+		for (int i = 0; i < contingut.length; i++) {
+			for (int j = 0; j < contingut[0].length; j++) {
+				if (contingut[i][j].length() > colMaxs[j]) {
+					colMaxs[j] = contingut[i][j].length();
+				}
+			}
+		}
+
+		for (int i = 0; i < colMaxs.length - 1; i++) {
+			colMaxs[i] += espaiEntreColumnes;
+		}
+		colMaxs[colMaxs.length - 1] += 1;
+
+		int spaces;
+		imprimir(" ");
+		for (int i = 0; i < titols.length; i++) {
+			spaces = colMaxs[i] - titols[i].length();
+			imprimir(titols[i]);
+			for (int j = 0; j < spaces; j++) {
+				imprimir(" ");
+			}
+		}
+		imprimirln("");
+		for (int i = 0; i < colMaxs.length; i++) {
+			for (int j = 0; j < colMaxs[i]; j++) {
+				imprimir("-");
+			}
+		}
+		imprimirln("-");
+		for (int i = 0; i < contingut.length; i++) {
+			imprimir(" ");
+			for (int j = 0; j < contingut[0].length; j++) {
+				spaces = colMaxs[j] - contingut[i][j].length();
+				imprimir(contingut[i][j]);
+				for (int k = 0; k < spaces; k++) {
+					imprimir(" ");
+				}
+			}
+			imprimirln("");
+		}
+	}
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Created by Oscar on 02/05/2016.
  */
 
 public class Principal {
+	private String agenda = "agenda.txt";
+
     public static void main(String[] args) {
         new Principal().inici();
     }
@@ -21,8 +25,66 @@ public class Principal {
 		int opcio = -1;
 		while (opcio != EXIT_OPTION) {
 			opcio = Biblioteca.menu(opcionsMenu, "Escull una opció: ");
+			switch (opcio) {
+				case 1:
+					inserirUsuari();
+					break;
+				case 2:
+					consultarUsuari();
+					break;
+				case 3:
+					mostrarUsuaris();
+					break;
+				case 4:
+					modificarUsuari();
+					break;
+				case 5:
+					eliminarUsuari();
+					break;
+			}
+
+			if (opcio != EXIT_OPTION) pause();
+			else exit();
 		}
     }
+
+	private void inserirUsuari() {
+
+	}
+
+	private void consultarUsuari() {
+
+	}
+
+	private void mostrarUsuaris() {
+		Usuari[] usuaris = FileIO.getUsuaris(agenda);
+		String[] titols = {"Id", "Nom", "Email"};
+		String[][] contingut = new String[usuaris.length][titols.length];
+		for (int i = 0; i < usuaris.length; i++) {
+			contingut[i][0] = usuaris[i].getId();
+			contingut[i][1] = usuaris[i].getNom() + " " + usuaris[i].getCognoms();
+			contingut[i][2] = usuaris[i].getEmail();
+		}
+		Biblioteca.imprimirTaula(titols, contingut);
+	}
+
+	private void modificarUsuari() {
+
+	}
+
+	private void eliminarUsuari() {
+
+	}
+
+	private void pause() {
+		Scanner sc = new Scanner(System.in);
+		Biblioteca.imprimir("Prèmer enter <-' per continuar...");
+		sc.nextLine();
+	}
+
+	private void exit() {
+		System.exit(0);
+	}
 }
 
 
