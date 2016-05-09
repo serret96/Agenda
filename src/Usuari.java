0d0479dc2a91b5e5919450c62e3fb9d9a4852be5
Kagnaska <kagnaska@gmail.com>
2016-05-09 10:48:41 +0200

    y

--- src/Usuari.java
+++ src/Usuari.java
@@ -2,12 +2,12 @@
  * Created by RUBEN on 02/05/2016.
  */
 public class Usuari {
-    private boolean valid;
-    private String id;
-    private String contrasenya;
-    private String nom;
-    private String cognoms;
-    private String email;
+    private static boolean valid;
+    private static String id;
+    private static String contrasenya;
+    private static String nom;
+    private static String cognoms;
+    private static String email;
 
     public boolean getValid(){return valid;}
     public String getId(){return id;}
@@ -15,12 +15,12 @@ public class Usuari {
     public String getNom(){return nom;}
     public String getCognoms(){return cognoms;}
     public String getEmail(){return email;}
-    public void setValid(boolean valid){valid = getValid();}
-    public void setId(String id){id = getId();}
-    public void setContrasenya(String contrasenya){contrasenya = getContrasenya();}
-    public void setNom(String nom){nom = getNom();}
-    public void setCognoms(String cognoms){cognoms = getCognoms();}
-    public void setEmail(String email){email = getEmail();}
+    public void setValid(boolean valid){Usuari.valid = valid;}
+    public void setId(String id){Usuari.id = id;}
+    public void setContrasenya(String contrasenya){Usuari.contrasenya = contrasenya;}
+    public void setNom(String nom){Usuari.nom = nom;}
+    public void setCognoms(String cognoms){Usuari.cognoms = cognoms;}
+    public void setEmail(String email){Usuari.email = email;}
     //private String generarContrasenya();
 
 }
