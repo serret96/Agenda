4fd1f912481a193b40e070ef38144ce5b92a5514
serret96 <ruben.serret@gmail.com>
2016-05-09 11:51:58 +0200

    Salutacions

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,33 +1,59 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Created by RUBEN on 02/05/2016.
  */
 public class FileIO {
+     Usuari usuari = new Usuari();
 
-    public static String[] getIdUsuaris(String fitxer){
+    public static ArrayList<String> getIdUsuaris(String fitxer) {
         File fin = new File(fitxer);
-
-        return Usuari.id;
-    }
-    public static Usuari getUsuari(String fitxer, String idUsuari){
-        File fin = new File(fitxer);
-        return ;
-    }
-    public static Usuari[] getUsuaris(String fitxer){
-        File fin = new File(fitxer);
-        return ;
-    }
-    public static boolean guardarUsuari(String fitxer, Usuari usuari){
-        File fin = new File(fitxer);
-        boolean resultat;
-        return ;
-    }
-    public static boolean borrarUsuari(String fitxer, String idUsuari){
-        File fin = new File(fitxer);
-        Boolean resultat;
-        return ;
-
+        Scanner sc = new Scanner(fin);
+        ArrayList<String> ids = new ArrayList<>();
+        while(sc.hasNextLine()){
+            String linia = sc.nextLine();
+            ids.add(linia.split(";")[1]);
+        }
+         return ids;
     }
 
+    public static Usuari getUsuari(String fitxer, String idUsuari) {
+        File fin = new File(fitxer);
+        return;
+    }
 
+    public static ArrayList<Usuari> getUsuaris(String fitxer) {
+        File fin = new File(fitxer);
+        Scanner sc = new Scanner(fin);
+        ArrayList<String> usuaris = new ArrayList<>();
+        while(sc.hasNextLine()){
+            String linia = sc.nextLine();
+            String[] u = linia.split(";");
+            Usuari user = new Usuari(u[0], u[1], u[2], u[3], u[4], u[5]);
+            usuaris.add(user);
+        }
+        return Usuari;
+    }
+
+    public static boolean guardarUsuari(String fitxer, Usuari usuari) {
+        File fin = new File(fitxer);
+        return resultatguardat();
+    }
+
+    public static boolean borrarUsuari(String fitxer, String idUsuari) {
+        File fin = new File(fitxer);
+        return resultatborrat();
+    }
+    public static boolean resultatguardat(){
+    boolean resultat = true;
+
+
+    return resultat;
+}
+    public static boolean resultatborrat(){
+     boolean resultat = true;
+
+        return resultat;
+    }
 }
